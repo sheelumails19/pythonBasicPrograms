b1f280735cf7ae4e5c82e3787bd482de157d910c
sheelumails19 <129538404+sheelumails19@users.noreply.github.com>
2023-12-27 11:35:32 -0800

    Create CountVowelsInString.py

---
+++ CountVowelsInString.py
@@ -0,0 +1,10 @@
+// Description: Write a function that counts the number of vowels in a given string.
+ 
+def count_vowels(s):
+    vowels = 'aeiouAEIOU'
+    return sum(1 for char in s if char in vowels)
+ 
+# Example usage:
+print(count_vowels('Hello World'))  # Output: 3
+
+// Output: 3
