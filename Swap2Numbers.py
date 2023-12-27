cbe4a1138b9cfba389d8c7e8e0f04045b71c3bdd
sheelumails19 <129538404+sheelumails19@users.noreply.github.com>
2023-12-27 11:42:41 -0800

    Create Swap2Numbers.py

---
+++ Swap2Numbers.py
@@ -0,0 +1,15 @@
+# Initial values
+a = 45
+b = 37
+
+# Swap variables
+a, b = b, a
+
+print(f'The value of a after swapping: {a}')
+print(f'The value of b after swapping: {b}')
+
+"""
+Output:
+The value of a after swapping: 37
+The value of b after swapping: 45
+"""
