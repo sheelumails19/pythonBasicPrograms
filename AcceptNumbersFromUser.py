bbe9db4e2aa9c3b3aa3991cd19f031bad60fbf57
sheelumails19 <129538404+sheelumails19@users.noreply.github.com>
2023-12-27 13:26:26 -0800

    Create AcceptNumbersFromUser.py

---
+++ AcceptNumbersFromUser.py
@@ -0,0 +1,27 @@
+// Write a program to accept two numbers from the user and calculate multiplication
+
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def sum(a, b):
+    return (a + b)
+    
+def mult(a, b):
+    return (a * b)
+    
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
+print(f'Sum of {a} and {b} is {mult(a, b)}')
+
+
+"""
+Output
+  
+Enter 1st number: 
+4 
+Enter 2nd number: 
+5 
+Sum of 4 and 5 is 9 Sum of 4 and 5 is 20
+"""
