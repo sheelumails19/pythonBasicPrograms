e2b889b2b66289ac39844ac5232f78919863df23
sheelumails19 <129538404+sheelumails19@users.noreply.github.com>
2023-12-27 11:42:01 -0800

    Update PrintRangeOfNumbers.py

--- PrintRangeOfNumbers.py
+++ PrintRangeOfNumbers.py
@@ -4,12 +4,11 @@ n = 5
 for i in range(n):
     print(f"The repeating elements are: {i}")
 
-
-/*
+"""
 Output:
 The repeating elements are: 0
 The repeating elements are: 1
 The repeating elements are: 2
 The repeating elements are: 3
 The repeating elements are: 4
-*/
+"""
