c54aa2f57c9d0df33002fd555c5f2f7ac265e104
sheelumails19 <129538404+sheelumails19@users.noreply.github.com>
2023-12-27 11:39:22 -0800

    Create CountWordsInSentence.py

---
+++ CountWordsInSentence.py
@@ -0,0 +1,12 @@
+// 	· Description: Write a function that counts the number of words in a sentence.
+ 
+
+def count_words(sentence):
+    words = sentence.split()
+    return len(words)
+ 
+# Example usage:
+print(count_words('This is a sample sentence.'))
+# Output: 5
+
+// Output: 5
