cd978202987cc9c3aec21559ec08fb2c6dd5210b
sheelumails19 <129538404+sheelumails19@users.noreply.github.com>
2023-12-27 13:28:14 -0800

    Create AcceptThreeStringFromOneInputCall.py

---
+++ AcceptThreeStringFromOneInputCall.py
@@ -0,0 +1,33 @@
+// Write a program to take three names as input from a user in the single input() function call.
+
+def main():
+    # Accept three names separated by spaces
+    names_input = input("Enter three names separated by spaces: ")
+
+    # Split the input string by spaces to get individual names
+    names = names_input.split()
+
+    # If the user provides fewer or more than three names, inform them
+    if len(names) != 3:
+        print("Please provide exactly three names separated by spaces.")
+        return
+
+    # Display the names entered by the user
+    print("Names entered:")
+    for name in names:
+        print(name)
+
+
+if __name__ == "__main__":
+    main()
+
+"""
+Output 
+
+Enter three names separated by spaces: 
+sag raj there
+Names entered:
+sag
+raj
+there
+"""
