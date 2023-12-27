6eb5a11206e639a863e50f40f9695064e21d1b1c
sheelumails19 <129538404+sheelumails19@users.noreply.github.com>
2023-12-27 11:28:27 -0800

    Create FindUniqueElementsInArray.py

---
+++ FindUniqueElementsInArray.py
@@ -0,0 +1,9 @@
+# Creating a list
+some_numbers = [1, 2, 2, 3, 1, 6, 4, 5, 4, 5, 5]
+ 
+# Finding unique elements using set
+unique_numbers = set(some_numbers)
+ 
+print("Unique Elements:", unique_numbers)
+
+// Unique Elements: {1, 2, 3, 4, 5, 6}
