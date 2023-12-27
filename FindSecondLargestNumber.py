b53d644fada8da942baf1e01583467fd2c0b7dd5
sheelumails19 <129538404+sheelumails19@users.noreply.github.com>
2023-12-27 11:38:34 -0800

    Create FindSecondLargestNumber.py

---
+++ FindSecondLargestNumber.py
@@ -0,0 +1,12 @@
+// 	· Description: Write a function to find the second largest number in an array.
+ 
+
+def second_largest_number(arr):
+    unique_sorted_arr = sorted(set(arr), reverse=True)
+    return unique_sorted_arr[1] if len(unique_sorted_arr) > 1 else None
+ 
+# Example usage:
+print(second_largest_number([5, 2, 7, 1, 8, 7]))
+# Output: 7
+
+// Output: 7
