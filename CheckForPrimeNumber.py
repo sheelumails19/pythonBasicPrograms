267eb7b44574dc7f8bc51ded5a5034d9c3459498
sheelumails19 <129538404+sheelumails19@users.noreply.github.com>
2023-12-27 11:36:23 -0800

    Create CheckForPrimeNumber.py

---
+++ CheckForPrimeNumber.py
@@ -0,0 +1,16 @@
+// 	· Description: Write a function to check if a given number is a prime number.
+ 
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+ 
+# Example usage:
+print(is_prime(7))  # Output: True
+
+
+// Output: True
