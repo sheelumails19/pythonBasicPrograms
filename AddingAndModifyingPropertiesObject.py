b66f0ec078351d77cd29c88693a0f048ffed9c84
sheelumails19 <129538404+sheelumails19@users.noreply.github.com>
2023-12-27 12:41:27 -0800

    Create AddingAndModifyingPropertiesObject.py

---
+++ AddingAndModifyingPropertiesObject.py
@@ -0,0 +1,17 @@
+person = {
+    'name': 'John',
+    'age': 30
+}
+
+# Adding a new key-value pair
+person['city'] = 'New York'
+
+# Modifying the value of an existing key
+person['age'] = 31
+
+# Printing the dictionary
+print(person)
+
+
+// Output: {'name': 'John', 'age': 31, 'city': 'New York'}
+
